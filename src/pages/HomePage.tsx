22bab8225e1fe09cedd2ab439be2c53432b65198
netta <nettashani@gmail.com>
2026-03-15 16:41:10 +0200

    minor

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,11 +21,11 @@ export const HomePage = () => {
         <div className="normal-color app-header-xl">
           <div className="margin-xl">שָׁלוֹם {name === "" ? "חֲבֵרָה" : name }</div>
           <img src={friendImageUrl} alt="App friend" height="256px" />
-          <div>מֵעַכְשָׁו אֲנִי אִתָּךְ</div>
+          <div>אֵיךְ אַתְּ מַרְגִּישָׁה?</div>
           <Link to="/feelings" className="app-header-m">
               <div>
                 <span><img src={ MediaUtil.getAppIcon(APP_ICONS.ARROW_RTL) } alt="" height="32px" /> </span>
-                הַקְלִיקִי כָּאן כְּדֵי לְהַמְשִׁיךְ
+                הַקְלִיקִי כָּאן כְּדֵי לְסַפֵּר לִי
               </div>
           </Link> 
         </div>
